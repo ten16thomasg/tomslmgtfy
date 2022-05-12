4a24675f4dc7a6b4af5a0853f7faa93f6054d77b
ten16thomasg <tm.personal@outlook.com>
2022-05-12 12:27:15 -0500

    updating blog posts

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,7 +4,7 @@ export const getGlobalData = () => {
     : 'Thomas Tyson-Gray';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Toms Blog';
+    : 'LMGTFY';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
